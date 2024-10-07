3f57ed6105cbf0dd4b76ea349706674fe039f919
olegov-lab <olego8760@gmail.com>
2024-10-07 21:40:04 +0300

    feat: add some functionality, e.g. Move to the next quiz, Progress

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from './Question';
+import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
@@ -43,9 +45,17 @@ function reducer(state, action) {
         ...state,
         answer: action.payload,
         points: action.payload === question.correctOption
-          ? state.points + 1
-          : state.points,
+          ? state.points + question.points
+          : state.points
       };
+
+    case 'nextQuestion':
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      }
+
     default:
       throw new Error('Action unknow');
   }
@@ -54,9 +64,10 @@ function reducer(state, action) {
 
 export default function App() {
 
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
 
   useEffect(function () {
     fetch('http://localhost:8000/questions')
@@ -79,11 +90,26 @@ export default function App() {
             dispatch={dispatch} />
         )}
         {status === 'active' &&
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />}
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+            />
+          </>
+        }
 
       </Main>
     </div>

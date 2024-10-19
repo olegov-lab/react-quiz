8f8ebc37ab1e07a84097dc50bcaae713c8aa4274
olegov-lab <olego8760@gmail.com>
2024-10-19 23:10:45 +0300

    feat: add Timer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useReducer } from 'react';
+
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
@@ -8,6 +9,10 @@ import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
 import FinishScreen from './FinishScreen';
+import Footer from './Footer';
+import Timer from './Timer';
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -18,6 +23,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -30,16 +36,20 @@ function reducer(state, action) {
         questions: action.payload,
         status: 'ready'
       };
+
     case 'dataFailed':
       return {
         ...state,
         status: 'error',
       };
+
     case 'start':
       return {
         ...state,
-        status: 'active'
+        status: 'active',
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
+
     case 'newAnswer':
       const question = state.questions.at(state.index);
 
@@ -70,6 +80,13 @@ function reducer(state, action) {
         ...initialState,
         questions: state.questions,
         status: 'ready',
+      };
+
+    case 'tick':
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
       }
 
     default:
@@ -80,7 +97,7 @@ function reducer(state, action) {
 
 export default function App() {
 
-  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
@@ -120,14 +137,18 @@ export default function App() {
               answer={answer}
             />
 
-            <footer>
+            <Footer>
+              <Timer
+                dispatch={dispatch}
+                secondsRemaining={secondsRemaining}
+              />
               <NextButton
                 dispatch={dispatch}
                 answer={answer}
                 index={index}
                 numQuestions={numQuestions}
               />
-            </footer>
+            </Footer>
           </>
         }
 
